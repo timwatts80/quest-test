f3ee01a38a09a157979ab559146fd8c944aed4f7
timwatts80 <55103053+timwatts80@users.noreply.github.com>
2023-03-24 11:50:48 -0600

    Imported from Quest

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import questTheme from 'src/MyDesignSystemLightTheme';
 import Frame2 from 'src/components/Frame2/Frame2';
-import AmlRoot from 'src/components/AmlRoot/AmlRoot';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <ThemeProvider theme={questTheme}>
         <Routes>
           <Route path="/" element={<Frame2 />} />
-          <Route path="AMLInvestorinfoPersonal_info" element={<AmlRoot />} />
         </Routes>
       </ThemeProvider>
     </StyledEngineProvider>
