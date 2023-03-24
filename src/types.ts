89149ff4b4a4ff17eb0cba27dfe4299f511782d3
timwatts80 <55103053+timwatts80@users.noreply.github.com>
2023-03-24 10:51:49 -0600

    Imported from Quest

--- src/types.ts
+++ src/types.ts
@@ -13,106 +13,132 @@
  *
  **********************************************************************/
 
+
 export interface SampleCardProps {
   className?: any;
   metadata: any;
 }
 
+
 export interface SampleCardGridProps {
   className?: any;
 }
+
 
 export interface Frame2Props {
   className?: any;
 }
 
+
 export interface AmlRootProps {
   className?: any;
 }
+
 
 export interface AltProBrowserTemplate1Props {
   className?: any;
 }
 
+
 export interface SearchProps {
   className?: any;
 }
+
 
 export interface BackProps {
   className?: any;
 }
 
+
 export interface UploadProps {
   className?: any;
 }
+
 
 export interface HelpProps {
   className?: any;
 }
 
+
 export interface SubNavSideProps {
   className?: any;
 }
+
 
 export interface CheckBoxCircleProps {
   className?: any;
 }
 
+
 export interface H4Props {
   className?: any;
 }
+
 
 export interface Body75RemProps {
   className?: any;
 }
 
+
 export interface BreadcrumbProps {
   className?: any;
 }
+
 
 export interface InvestorInfoEntityProps {
   className?: any;
 }
 
+
 export interface EntityTypeProps {
   className?: any;
 }
+
 
 export interface SelectFieldProps {
   className?: any;
 }
 
+
 export interface MenuDownProps {
   className?: any;
 }
+
 
 export interface AccountNameProps {
   className?: any;
 }
 
+
 export interface InputFieldProps {
   className?: any;
 }
+
 
 export interface TaxIdTypeProps {
   className?: any;
 }
 
+
 export interface IdNumberProps {
   className?: any;
 }
+
 
 export interface ForeignInvestorProps {
   className?: any;
 }
 
+
 export interface AccreditationProps {
   className?: any;
 }
 
+
 export interface AmlMenuInvestorProps {
   className?: any;
 }
+
 
 export interface MenuItemProps {
   className?: any;
