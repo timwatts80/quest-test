f8fb5bd27dcf86b96cabfcd2e81227270c710929
timwatts80 <55103053+timwatts80@users.noreply.github.com>
2023-03-24 20:30:19 +0000

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import questTheme from 'src/MyDesignSystemLightTheme';
 import SubNavSide from 'src/components/SubNavSide/SubNavSide';
 import SubNav from 'src/components/SubNav/SubNav';
+import SampleCard from './components/SampleCard/SampleCard';
 
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={questTheme}>
         <Routes>
-          <Route path="sub_nav" element={<SubNav />} />
+          <Route path="/" element={<SubNav />} />
         </Routes>
       </ThemeProvider>
     </StyledEngineProvider>
